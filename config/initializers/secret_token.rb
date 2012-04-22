5351aff7b3905687df06db51828bb3a0856858c6
Joseph Barrow <josephbarrow@new-host-6.home>
2012-04-21 20:21:19 -0400

    Rails Skeleton for FauxTwit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fauxtwit::Application.config.secret_token = 'b561bcc18eef25356f86a7c65552088bec4c438a37776165430cad80a3a4b86e428fa519b4b475e3a26ea7423081cac3aa9f5bca6162c4328fa170faba2ad01a'
