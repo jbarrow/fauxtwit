6706406db6c0f110c7e640df4473dac1bc8e4c59
Joseph Barrow <josephbarrow@new-host-6.home>
2012-06-03 13:57:25 -0400

    Let's hope

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,4 @@
 Fauxtwit::Application.routes.draw do
-  root to: "static_pages#home"
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destroy]
-
-  match "/about", to: "static_pages#about"
-  match "/help", to: "static_pages#help"
-  match "/contact", to: "static_pages#contact"
-
-  match "/signup", to: "users#new"
-  match "/signin", to: "sessions#new"
-  match "/signout", to: "sessions#destroy", via: :delete
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
